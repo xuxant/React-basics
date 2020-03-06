74dee0b74db2786b71b01960a166498fadcc75ed
Susanta Gautam <xuxant02@gmail.com>
2020-03-06 11:10:05 +0545

    Updates

---
+++ precache-manifest.df7aae77a86d04cb61b65af6ff0d4708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c311d21beb6fd6ca2569f4275f72cc81",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "dac60dbbc9610d38ad00",
+    "url": "/monsters/static/css/main.a0a243a0.chunk.css"
+  },
+  {
+    "revision": "570f35dee691a275e91e",
+    "url": "/monsters/static/js/2.b293cc3b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.b293cc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac60dbbc9610d38ad00",
+    "url": "/monsters/static/js/main.586e3c6b.chunk.js"
+  },
+  {
+    "revision": "a503c84b12d8298e6ab9",
+    "url": "/monsters/static/js/runtime-main.66da3aa8.js"
+  }
+]);
